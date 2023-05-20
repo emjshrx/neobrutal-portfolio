b7a83b1f246668d1d626a622172521e7212b946f
Emil Joshy <emjshrx@gmail.com>
2023-05-20 17:03:29 +0530

    Add projects

---
+++ src/components/Projects.tsx
@@ -0,0 +1,26 @@
+import ProjectTile from "./projectTile";
+type ProjectType = {
+  name: string;
+  description: string;
+  html_url: string;
+};
+
+const Projects = ({ projectsList }: { projectsList: ProjectType[] }) => {
+  return (
+    <div className="h-full w-full p-[5%] grid gap-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      {projectsList.map((project, index) => {
+        return (
+          <a href={project.html_url} key={project.html_url} target="_blank">
+            <ProjectTile
+              variant={index % 2}
+              name={project.name}
+              desc={project.description}
+            />
+          </a>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Projects;
---
+++ src/components/projectTile.tsx
@@ -0,0 +1,35 @@
+const ProjectTile = ({
+  variant,
+  name,
+  desc,
+}: {
+  variant: number;
+  name: string;
+  desc: string;
+}) => {
+  return (
+    <div className="w-full relative flex justify-center items-center flex-col px-[20%] group">
+      <div className="w-full aspect-square rounded-lg shadow-hard border-2 border-black flex justify-center items-center relative ">
+        <div
+          style={{
+            backgroundImage: `url(https://raw.githubusercontent.com/emjshrx/${name}/main/img/thumbnail.png)`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+          }}
+          className="w-full h-full bg-black bg-blend-hard-light group-hover:opacity-100 opacity-80"
+        />
+        <div
+          className={`${
+            variant == 1 ? "bg-green text-black" : "bg-black text-green"
+          } absolute w-full shadow-hard  h-fit border-2 rounded-lg border-black p-2 text-sm bottom-[15%] -translate-x-[25%]`}
+        >
+          {name}
+        </div>
+      </div>
+      <div className="mt-3 text-xs p-1">{desc}</div>
+    </div>
+  );
+};
+
+export default ProjectTile;
